e0a125298a3fe5591c2f3e53036a89c6b0b8cf00
TomaszOrpik <47402951+TOrpik@users.noreply.github.com>
2019-12-04 23:13:12 +0100

    task 1/2

---
+++ js/zadanie02.js
@@ -0,0 +1,10 @@
+function concatArray(array1, array2) {
+    return array1.concat(array2);
+}
+
+var arr1 = ["pomidor", "ogorek"];
+var arr2 = ["cebula", "dorsz"];
+
+console.log(arr1);
+console.log(arr2);
+console.log(concatArray(arr1, arr2));
